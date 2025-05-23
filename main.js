7a7defb7ae767acdcf9b19cadd14a302d6db9c9a
Felgi <felipeferraridoval@gmail.com>
2025-05-22 21:42:18 -0300

    terminado

--- main.js
+++ main.js
@@ -2,64 +2,64 @@
 
 let alumnos = [
     {
-        nombre: "Matias",
-        apellido: "Gonzalez",
+        nombre: "matias",
+        apellido: "gonzalez",
         dni: 48945678,
         anio: 4,
         curso: "A",
         nota: 8,
     },
     {
-        nombre: "Martina",
-        apellido: "Lopez",
+        nombre: "martina",
+        apellido: "lopez",
         dni: 48112345,
         anio: 4,
         curso: "A",
         nota: 9,
     },
     {
-        nombre: "Pedro",
-        apellido: "Martinez",
+        nombre: "pedro",
+        apellido: "martinez",
         dni: 48349678,
         anio: 5,
         curso: "A",
         nota: 7,
     },
     {
-        nombre: "Ana",
-        apellido: "Henriquez",
+        nombre: "ana",
+        apellido: "henriquez",
         dni: 48052678,
         anio: 4,
         curso: "B",
         nota: 5,
     },
     {
-        nombre: "Juan",
-        apellido: "Gomez",
+        nombre: "juan",
+        apellido: "gomez",
         dni: 48110315,
         anio: 4,
         curso: "B",
         nota: 4,
     },
     {
-        nombre: "Martina",
-        apellido: "Diaz",
+        nombre: "martina",
+        apellido: "diaz",
         dni: 48949478,
         anio: 5,
         curso: "A",
         nota: 5,
     },
     {
-        nombre: "Leonel",
-        apellido: "Rodriguez",
+        nombre: "leonel",
+        apellido: "rodriguez",
         dni: 48941178,
         anio: 3,
         curso: "B",
         nota: 9,
     },
     {
-        nombre: "Felipe",
-        apellido: "Romero",
+        nombre: "felipe",
+        apellido: "romero",
         dni: 48969078,
         anio: 3,
         curso: "B",
@@ -67,15 +67,57 @@ let alumnos = [
     }
 ]
 
+// Ejercicio 2: Capitalizar function
+
 function Capitalizar(palabra) {
     let palabraCapitalizada = palabra.charAt(0).toUpperCase() + palabra.slice(1);
-    console.log(palabraCapitalizada);
     return palabraCapitalizada;
 }
 
+// Ejercicio 3: MostrarAlumnos function
+
 function MostrarAlumnos(alumnos) {
     alumnos.forEach(alumno => {
-        console.log(`DNI: ${alumno.dni} \nNombre y apellido: ${alumno.nombre} ${alumno.apellido}\nCurso: ${alumno.anio}°${alumno.curso}\nNota: ${alumno.nota}\n`);
+        stringDni = alumno.dni.toString();
+        stringDni = stringDni.slice(0, 2) + "." + stringDni.slice(2, 5) + "." + stringDni.slice(5, 8);
+        console.log(`DNI: ${stringDni} \nNombre y apellido: ${alumno.nombre} ${alumno.apellido}\nCurso: ${alumno.anio}°${alumno.curso}\nNota: ${alumno.nota}\n`);
     });
 }
+
+// Ejercicio 4: Capitzalizar los nombres y apellidos de los alumnos
+
+function CapitalizarAlumnos(alumnos) {
+    alumnos.forEach(alumno => {
+        alumno.nombre = Capitalizar(alumno.nombre);
+        alumno.apellido = Capitalizar(alumno.apellido);
+    });
+}
+CapitalizarAlumnos(alumnos);
+
+// Ejercicio 5: Mostrar alumnos
+
 MostrarAlumnos(alumnos);
+
+// Ejercicio 6: Filtrar alumnos
+
+function FiltrarAlumnosAprobados(alumnos) {
+    let alumnosAprobados = alumnos.filter(alumno => alumno.nota >= 6);
+    console.log("Alumnos aprobados:\n");
+    MostrarAlumnos(alumnosAprobados);
+}
+
+function FiltrarAlumnos4to(alumnos) {
+    let alumnos4to = alumnos.filter(alumno => alumno.anio = 4);
+    console.log("Alumnos de 4to año:\n");
+    MostrarAlumnos(alumnos4to);
+}
+
+function FiltrarAlumnosCursoB(alumnos) {
+    let alumnosCursoB = alumnos.filter(alumno => alumno.curso == "B");
+    console.log("Alumnos del B:\n");
+    MostrarAlumnos(alumnosCursoB);
+}
+
+FiltrarAlumnosAprobados(alumnos);
+FiltrarAlumnos4to(alumnos);
+FiltrarAlumnosCursoB(alumnos);
